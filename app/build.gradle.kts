30a57a2a439f8cc7b2886f735d3af5a2ee58b501
abhishekrao <abhishekk57@birlasoft.com>
2024-03-04 20:59:59 +0530

    AR Scene view is done.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,15 +40,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+//    implementation ("com.android.support:appcompat-v7:28.0.0")
+//    implementation ("com.android.support:support-v4:28.0.0")
+//    implementation("androidx.core:core-ktx:1.12.0")
+//    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation ("androidx.transition:transition-ktx:1.4.1")
-    implementation ("com.google.ar.sceneform:animation:1.17.1")
-    implementation("com.google.ar.sceneform:core:1.17.1")
+//    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+//    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("com.google.ar.sceneform.ux:sceneform-ux:1.17.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/arscenedemo/ArActivity.kt
@@ -0,0 +1,121 @@
+package com.example.arscenedemo
+
+import android.app.Activity
+import android.app.ActivityManager
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+import java.util.Objects
+
+
+class ArActivity : AppCompatActivity() {
+    private var clickNo = 0
+    private var arCam: ArFragment? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ar)
+        runSceneView()
+    }
+
+   protected fun runSceneView(){
+        if (checkSystemSupport(this)) {
+            arCam = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment?
+            assert(arCam != null)
+            arCam!!.setOnTapArPlaneListener { hitResult, _, _ ->
+                clickNo++
+                if (clickNo == 1) {
+                    val anchor: Anchor = hitResult.createAnchor()
+                    ModelRenderable.builder()
+                        .setSource(this, R.raw.abc) // set glb model
+                        .build()
+                        .thenAccept { modelRenderable -> addModel(anchor, modelRenderable) }
+                        .exceptionally { throwable ->
+                            val builder =
+                                AlertDialog.Builder(this)
+                            builder.setMessage("Something is not right" + throwable.message)
+                                .show()
+                            null
+                        }
+                }
+            }
+        }
+    }
+    private fun permission(){
+        if (ContextCompat.checkSelfPermission(
+                this,
+                "Manifest.permission.CAMERA"
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            runSceneView()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf("Manifest.permission.CAMERA"),
+                CAMERA_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+    private fun addModel(anchor: Anchor, modelRenderable: ModelRenderable) {
+        val anchorNode = AnchorNode(anchor)
+        anchorNode.setParent(arCam!!.getArSceneView().getScene())
+        val model = TransformableNode(arCam!!.getTransformationSystem())
+        model.setParent(anchorNode)
+        model.setRenderable(modelRenderable)
+        model.select()
+    }
+
+    companion object {
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 100
+        fun checkSystemSupport(activity: Activity): Boolean {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                val openGlVersion = (Objects.requireNonNull(
+                    activity.getSystemService(
+                        ACTIVITY_SERVICE
+                    )
+                ) as ActivityManager).deviceConfigurationInfo.glEsVersion
+                if (openGlVersion.toDouble() >= 3.0) {
+                    true
+                } else {
+                    Toast.makeText(
+                        activity,
+                        "App needs OpenGl Version 3.0 or later",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    activity.finish()
+                    false
+                }
+            } else {
+                Toast.makeText(
+                    activity,
+                    "App does not support required Build Version",
+                    Toast.LENGTH_SHORT
+                ).show()
+                activity.finish()
+                false
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                runSceneView()
+            }
+        }
+    }
+}
